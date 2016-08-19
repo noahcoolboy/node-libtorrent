a607ff67dc92c6ac07702b969fbfc0e597c487ba
Alex R <lyssdod@gmail.com>
2016-08-20 00:44:03 +0300

    alert converted to nan2

--- src/alert.cpp
+++ src/alert.cpp
@@ -1,4 +1,5 @@
 #include <v8.h>
+#include <nan.h>
 #include <node.h>
 
 #include <libtorrent/alert.hpp>
@@ -10,46 +11,49 @@ using namespace node;
 
 
 namespace nodelt {
-  v8::Local<v8::Object> alert_to_object(const libtorrent::alert& alert) {
-    HandleScope scope;
-    Local<Object> obj = Object::New();
+    v8::Local<v8::Object> alert_to_object(const libtorrent::alert& alert) {
+        Nan::EscapableHandleScope scope;
+        Local<Object> obj = Nan::New<Object>();
 
-    obj->Set(String::NewSymbol("message"), String::New(alert.message().c_str()));
-    obj->Set(String::NewSymbol("what"), String::New(alert.what()));
-    obj->Set(String::NewSymbol("category"), Integer::New(alert.category()));
-    return scope.Close(obj);
-  };
+        obj->Set(Nan::New("message").ToLocalChecked(), Nan::New(alert.message()).ToLocalChecked());
+        obj->Set(Nan::New("what").ToLocalChecked(), Nan::New(alert.what()).ToLocalChecked());
+        obj->Set(Nan::New("category").ToLocalChecked(), Nan::New<Integer>(alert.category()));
 
-  void bind_alert(Handle<Object> target) {
-    Local<Object> category_t = Object::New();
-    category_t->Set(String::NewSymbol("error_notification"),
-      Integer::New(libtorrent::alert::error_notification));
-    category_t->Set(String::NewSymbol("peer_notification"),
-      Integer::New(libtorrent::alert::peer_notification));
-    category_t->Set(String::NewSymbol("port_mapping_notification"),
-      Integer::New(libtorrent::alert::port_mapping_notification));
-    category_t->Set(String::NewSymbol("storage_notification"),
-      Integer::New(libtorrent::alert::storage_notification));
-    category_t->Set(String::NewSymbol("tracker_notification"),
-      Integer::New(libtorrent::alert::tracker_notification));
-    category_t->Set(String::NewSymbol("debug_notification"),
-      Integer::New(libtorrent::alert::debug_notification));
-    category_t->Set(String::NewSymbol("status_notification"),
-      Integer::New(libtorrent::alert::status_notification));
-    category_t->Set(String::NewSymbol("progress_notification"),
-      Integer::New(libtorrent::alert::progress_notification));
-    category_t->Set(String::NewSymbol("ip_block_notification"),
-      Integer::New(libtorrent::alert::ip_block_notification));
-    category_t->Set(String::NewSymbol("performance_warning"),
-      Integer::New(libtorrent::alert::performance_warning));
-    category_t->Set(String::NewSymbol("dht_notification"),
-      Integer::New(libtorrent::alert::dht_notification));
-    category_t->Set(String::NewSymbol("stats_notification"),
-      Integer::New(libtorrent::alert::stats_notification));
-    category_t->Set(String::NewSymbol("rss_notification"),
-      Integer::New(libtorrent::alert::rss_notification));
-    category_t->Set(String::NewSymbol("all_categories"),
-      Integer::New(libtorrent::alert::all_categories));
-    target->Set(String::NewSymbol("category_t"), category_t);
+        return scope.Escape(obj);
+    };
+
+    void bind_alert(Local<Object> target) {
+        Local<Object> category_t = Nan::New<Object>();
+
+        category_t->Set(Nan::New("error_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::error_notification));
+        category_t->Set(Nan::New("peer_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::peer_notification));
+        category_t->Set(Nan::New("port_mapping_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::port_mapping_notification));
+        category_t->Set(Nan::New("storage_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::storage_notification));
+        category_t->Set(Nan::New("tracker_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::tracker_notification));
+        category_t->Set(Nan::New("debug_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::debug_notification));
+        category_t->Set(Nan::New("status_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::status_notification));
+        category_t->Set(Nan::New("progress_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::progress_notification));
+        category_t->Set(Nan::New("ip_block_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::ip_block_notification));
+        category_t->Set(Nan::New("performance_warning").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::performance_warning));
+        category_t->Set(Nan::New("dht_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::dht_notification));
+        category_t->Set(Nan::New("stats_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::stats_notification));
+        category_t->Set(Nan::New("rss_notification").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::rss_notification));
+        category_t->Set(Nan::New("all_categories").ToLocalChecked(),
+            Nan::New<Integer>(libtorrent::alert::all_categories));
+
+        target->Set(Nan::New("category_t").ToLocalChecked(), category_t);
   };
 }; // namespace nodelt
