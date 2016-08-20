13b85f0935b756d21bc373f74aa62d23c038e2e6
Alex R <lyssdod@gmail.com>
2016-08-20 21:22:23 +0300

    peer_info converted to nan2

--- src/peer_info.cpp
+++ src/peer_info.cpp
@@ -1,4 +1,5 @@
 #include <v8.h>
+#include <nan.h>
 #include <node.h>
 
 #include <libtorrent/peer_info.hpp>
@@ -8,136 +9,137 @@
 using namespace v8;
 using namespace node;
 
-
 namespace nodelt {
-  Local<Array> get_tcp_endpoint(const libtorrent::tcp::endpoint& ep) {
-    HandleScope scope;
+    void bind_peer_info(Local<Object> target) {
+        // set flags
+        Local<Object> obj = Nan::New<Object>();
 
-    Local<Array> ret = Array::New();
-    ret->Set(0, String::New(ep.address().to_string().c_str()));
-    ret->Set(1, Integer::New(ep.port()));
-    return scope.Close(ret);
-  };
+        obj->Set(Nan::New("interesting").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::interesting));
+        obj->Set(Nan::New("choked").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::choked));
+        obj->Set(Nan::New("remote_interested").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::remote_interested));
+        obj->Set(Nan::New("remote_choked").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::remote_choked));
+        obj->Set(Nan::New("supports_extensions").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::supports_extensions));
+        obj->Set(Nan::New("local_connection").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::local_connection));
+        obj->Set(Nan::New("handshake").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::handshake));
+        obj->Set(Nan::New("connecting").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::connecting));
+        obj->Set(Nan::New("on_parole").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::on_parole));
+        obj->Set(Nan::New("seed").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::seed));
+        obj->Set(Nan::New("optimistic_unchoke").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::optimistic_unchoke));
+        obj->Set(Nan::New("snubbed").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::snubbed));
+        obj->Set(Nan::New("upload_only").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::upload_only));
+        obj->Set(Nan::New("endgame_mode").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::endgame_mode));
+        obj->Set(Nan::New("holepunched").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::holepunched));
 
-  Local<Array> get_pieces(const libtorrent::peer_info& pi) {
-    HandleScope scope;
+#ifndef TORRENT_DISABLE_ENCRYPTION
+        obj->Set(Nan::New("rc4_encrypted").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::rc4_encrypted));
+        obj->Set(Nan::New("plaintext_encrypted").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::plaintext_encrypted));
+#endif
 
-    Local<Array> ret = Array::New();
-    for (libtorrent::bitfield::const_iterator
-         i(pi.pieces.begin()), e(pi.pieces.end()); i != e; ++i)
-      ret->Set(ret->Length(), Integer::New(*i));
-    return scope.Close(ret);
-  };
+        obj->Set(Nan::New("standard_bittorrent").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::standard_bittorrent));
+        obj->Set(Nan::New("web_seed").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::web_seed));
 
-  Local<Object> peer_info_to_object(const libtorrent::peer_info& pi) {
-    HandleScope scope;
-    Local<Object> obj = Object::New();
+        obj->Set(Nan::New("tracker").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::tracker));
+        obj->Set(Nan::New("dht").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::dht));
+        obj->Set(Nan::New("pex").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::pex));
+        obj->Set(Nan::New("lsd").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::lsd));
+        obj->Set(Nan::New("resume_data").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::resume_data));
+        obj->Set(Nan::New("incoming").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::incoming));
 
-    obj->Set(String::NewSymbol("falgs"), Uint32::New(pi.flags));
-    obj->Set(String::NewSymbol("source"), Integer::New(pi.source));
-    obj->Set(String::NewSymbol("read_state"), Integer::New(pi.read_state));
-    obj->Set(String::NewSymbol("write_state"), Integer::New(pi.write_state));
-    obj->Set(String::NewSymbol("ip"), get_tcp_endpoint(pi.ip));
-    obj->Set(String::NewSymbol("up_speed"), Integer::New(pi.up_speed));
-    obj->Set(String::NewSymbol("down_speed"), Integer::New(pi.down_speed));
-    obj->Set(String::NewSymbol("payload_up_speed"), Integer::New(pi.payload_up_speed));
-    obj->Set(String::NewSymbol("payload_down_speed"), Integer::New(pi.payload_down_speed));
-    obj->Set(String::NewSymbol("total_download"), Integer::New(pi.total_download));
-    obj->Set(String::NewSymbol("total_upload"), Integer::New(pi.total_upload));
-    obj->Set(String::NewSymbol("pid"),
-      String::New(libtorrent::to_hex(pi.pid.to_string()).c_str()));
-    obj->Set(String::NewSymbol("pieces"), get_pieces(pi));
-    obj->Set(String::NewSymbol("upload_limit"), Integer::New(pi.upload_limit));
-    obj->Set(String::NewSymbol("download_limit"), Integer::New(pi.download_limit));
-    obj->Set(String::NewSymbol("last_request"),
-      Integer::New(libtorrent::total_seconds(pi.last_request)));
-    obj->Set(String::NewSymbol("last_active"),
-      Integer::New(libtorrent::total_seconds(pi.last_active)));
-    obj->Set(String::NewSymbol("download_queue_time"),
-      Integer::New(libtorrent::total_seconds(pi.download_queue_time)));
-    obj->Set(String::NewSymbol("queue_bytes"), Integer::New(pi.queue_bytes));
-    obj->Set(String::NewSymbol("request_timeout"), Integer::New(pi.request_timeout));
-    obj->Set(String::NewSymbol("send_buffer_size"), Integer::New(pi.send_buffer_size));
-    obj->Set(String::NewSymbol("used_send_buffer"), Integer::New(pi.used_send_buffer));
-    obj->Set(String::NewSymbol("receive_buffer_size"), Integer::New(pi.receive_buffer_size));
-    obj->Set(String::NewSymbol("used_receive_buffer"), Integer::New(pi.used_receive_buffer));
-    obj->Set(String::NewSymbol("num_hashfails"), Integer::New(pi.num_hashfails));
+        obj->Set(Nan::New("bw_idle").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::bw_idle));
+        obj->Set(Nan::New("bw_limit").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::bw_limit));
+        obj->Set(Nan::New("bw_network").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::bw_network));
+        obj->Set(Nan::New("bw_disk").ToLocalChecked(), Nan::New<Integer>(libtorrent::peer_info::bw_disk));
+
+        target->Set(Nan::New("peer_info").ToLocalChecked(), obj);
+    };
+
+    Local<Array> get_tcp_endpoint(const libtorrent::tcp::endpoint& ep) {
+        Nan::EscapableHandleScope scope;
+
+        Local<Array> ret = Nan::New<Array>();
+
+        ret->Set(0, Nan::New<String>(ep.address().to_string()).ToLocalChecked());
+        ret->Set(1, Nan::New<Integer>(ep.port()));
+
+        return scope.Escape(ret);
+    };
+
+    Local<Array> get_pieces(const libtorrent::peer_info& pi) {
+        Nan::EscapableHandleScope scope;
+
+        Local<Array> ret = Nan::New<Array>();
+
+        for (libtorrent::bitfield::const_iterator i(pi.pieces.begin()), e(pi.pieces.end()); i != e; ++i)
+            ret->Set(ret->Length(), Nan::New<Integer>(*i));
+
+        return scope.Escape(ret);
+    };
+
+    Local<Object> peer_info_to_object(const libtorrent::peer_info& pi) {
+        Nan::EscapableHandleScope scope;
+
+        Local<Object> obj = Nan::New<Object>();
+
+        obj->Set(Nan::New("falgs").ToLocalChecked(), Nan::New<Uint32>(pi.flags));
+        obj->Set(Nan::New("source").ToLocalChecked(), Nan::New<Integer>(pi.source));
+        obj->Set(Nan::New("read_state").ToLocalChecked(), Nan::New<Integer>(pi.read_state));
+        obj->Set(Nan::New("write_state").ToLocalChecked(), Nan::New<Integer>(pi.write_state));
+        obj->Set(Nan::New("ip").ToLocalChecked(), get_tcp_endpoint(pi.ip));
+        obj->Set(Nan::New("up_speed").ToLocalChecked(), Nan::New<Integer>(pi.up_speed));
+        obj->Set(Nan::New("down_speed").ToLocalChecked(), Nan::New<Integer>(pi.down_speed));
+        obj->Set(Nan::New("payload_up_speed").ToLocalChecked(), Nan::New<Integer>(pi.payload_up_speed));
+        obj->Set(Nan::New("payload_down_speed").ToLocalChecked(), Nan::New<Integer>(pi.payload_down_speed));
+        obj->Set(Nan::New("total_download").ToLocalChecked(), Nan::New<Number>(pi.total_download));
+        obj->Set(Nan::New("total_upload").ToLocalChecked(), Nan::New<Number>(pi.total_upload));
+        obj->Set(Nan::New("pid").ToLocalChecked(), Nan::New<String>(libtorrent::to_hex(pi.pid.to_string())).ToLocalChecked());
+        obj->Set(Nan::New("pieces").ToLocalChecked(), get_pieces(pi));
+        obj->Set(Nan::New("upload_limit").ToLocalChecked(), Nan::New<Integer>(pi.upload_limit));
+        obj->Set(Nan::New("download_limit").ToLocalChecked(), Nan::New<Integer>(pi.download_limit));
+        obj->Set(Nan::New("last_request").ToLocalChecked(), Nan::New<Integer>(libtorrent::total_seconds(pi.last_request)));
+        obj->Set(Nan::New("last_active").ToLocalChecked(), Nan::New<Integer>(libtorrent::total_seconds(pi.last_active)));
+        obj->Set(Nan::New("download_queue_time").ToLocalChecked(), Nan::New<Integer>(libtorrent::total_seconds(pi.download_queue_time)));
+        obj->Set(Nan::New("queue_bytes").ToLocalChecked(), Nan::New<Integer>(pi.queue_bytes));
+        obj->Set(Nan::New("request_timeout").ToLocalChecked(), Nan::New<Integer>(pi.request_timeout));
+        obj->Set(Nan::New("send_buffer_size").ToLocalChecked(), Nan::New<Integer>(pi.send_buffer_size));
+        obj->Set(Nan::New("used_send_buffer").ToLocalChecked(), Nan::New<Integer>(pi.used_send_buffer));
+        obj->Set(Nan::New("receive_buffer_size").ToLocalChecked(), Nan::New<Integer>(pi.receive_buffer_size));
+        obj->Set(Nan::New("used_receive_buffer").ToLocalChecked(), Nan::New<Integer>(pi.used_receive_buffer));
+        obj->Set(Nan::New("num_hashfails").ToLocalChecked(), Nan::New<Integer>(pi.num_hashfails));
+
 #ifndef TORRENT_DISABLE_RESOLVE_COUNTRIES
-    obj->Set(String::NewSymbol("country"), String::New(pi.country));
+        obj->Set(Nan::New("country").ToLocalChecked(), Nan::New<String>(pi.country).ToLocalChecked());
 #endif
 #ifndef TORRENT_DISABLE_GEO_IP
-    obj->Set(String::NewSymbol("inet_as_name"), String::New(pi.inet_as_name.c_str()));
-    obj->Set(String::NewSymbol("inet_as"), Integer::New(pi.inet_as));
+        obj->Set(Nan::New("inet_as_name").ToLocalChecked(), Nan::New<String>(pi.inet_as_name).ToLocalChecked());
+        obj->Set(Nan::New("inet_as").ToLocalChecked(), Nan::New<Integer>(pi.inet_as));
 #endif
-    // obj->Set(String::NewSymbol("load_balancing"), Integer::New(pi.load_balancing));
-    obj->Set(String::NewSymbol("download_queue_length"), Integer::New(pi.download_queue_length));
-    obj->Set(String::NewSymbol("timed_out_requests"), Integer::New(pi.timed_out_requests));
-    obj->Set(String::NewSymbol("busy_requests"), Integer::New(pi.busy_requests));
-    obj->Set(String::NewSymbol("requests_in_buffer"), Integer::New(pi.requests_in_buffer));
-    obj->Set(String::NewSymbol("target_dl_queue_length"), Integer::New(pi.target_dl_queue_length));
-    obj->Set(String::NewSymbol("upload_queue_length"), Integer::New(pi.upload_queue_length));
-    obj->Set(String::NewSymbol("failcount"), Integer::New(pi.failcount));
-    obj->Set(String::NewSymbol("downloading_piece_index"), Integer::New(pi.downloading_piece_index));
-    obj->Set(String::NewSymbol("downloading_block_index"), Integer::New(pi.downloading_block_index));
-    obj->Set(String::NewSymbol("downloading_progress"), Integer::New(pi.downloading_progress));
-    obj->Set(String::NewSymbol("downloading_total"), Integer::New(pi.downloading_total));
-    obj->Set(String::NewSymbol("client"), String::New(pi.client.c_str()));
-    obj->Set(String::NewSymbol("connection_type"), Integer::New(pi.connection_type));
-    obj->Set(String::NewSymbol("remote_dl_rate"), Integer::New(pi.remote_dl_rate));
-    obj->Set(String::NewSymbol("pending_disk_bytes"), Integer::New(pi.pending_disk_bytes));
-    obj->Set(String::NewSymbol("send_quota"), Integer::New(pi.send_quota));
-    obj->Set(String::NewSymbol("receive_quota"), Integer::New(pi.receive_quota));
-    obj->Set(String::NewSymbol("rtt"), Integer::New(pi.rtt));
-    obj->Set(String::NewSymbol("num_pieces"), Integer::New(pi.num_pieces));
-    obj->Set(String::NewSymbol("download_rate_peak"), Integer::New(pi.download_rate_peak));
-    obj->Set(String::NewSymbol("upload_rate_peak"), Integer::New(pi.upload_rate_peak));
-    obj->Set(String::NewSymbol("progress"), Number::New(pi.progress));
-    obj->Set(String::NewSymbol("progress_ppm"), Integer::New(pi.progress_ppm));
-    obj->Set(String::NewSymbol("estimated_reciprocation_rate"), Integer::New(pi.estimated_reciprocation_rate));
-    obj->Set(String::NewSymbol("local_endpoint"), get_tcp_endpoint(pi.local_endpoint));
+        // obj->Set(Nan::New("load_balancing").ToLocalChecked(), Nan::New<Integer>(pi.load_balancing));
+        obj->Set(Nan::New("download_queue_length").ToLocalChecked(), Nan::New<Integer>(pi.download_queue_length));
+        obj->Set(Nan::New("timed_out_requests").ToLocalChecked(), Nan::New<Integer>(pi.timed_out_requests));
+        obj->Set(Nan::New("busy_requests").ToLocalChecked(), Nan::New<Integer>(pi.busy_requests));
+        obj->Set(Nan::New("requests_in_buffer").ToLocalChecked(), Nan::New<Integer>(pi.requests_in_buffer));
+        obj->Set(Nan::New("target_dl_queue_length").ToLocalChecked(), Nan::New<Integer>(pi.target_dl_queue_length));
+        obj->Set(Nan::New("upload_queue_length").ToLocalChecked(), Nan::New<Integer>(pi.upload_queue_length));
+        obj->Set(Nan::New("failcount").ToLocalChecked(), Nan::New<Integer>(pi.failcount));
+        obj->Set(Nan::New("downloading_piece_index").ToLocalChecked(), Nan::New<Integer>(pi.downloading_piece_index));
+        obj->Set(Nan::New("downloading_block_index").ToLocalChecked(), Nan::New<Integer>(pi.downloading_block_index));
+        obj->Set(Nan::New("downloading_progress").ToLocalChecked(), Nan::New<Integer>(pi.downloading_progress));
+        obj->Set(Nan::New("downloading_total").ToLocalChecked(), Nan::New<Integer>(pi.downloading_total));
+        obj->Set(Nan::New("client").ToLocalChecked(), Nan::New<String>(pi.client).ToLocalChecked());
+        obj->Set(Nan::New("connection_type").ToLocalChecked(), Nan::New<Integer>(pi.connection_type));
+        obj->Set(Nan::New("remote_dl_rate").ToLocalChecked(), Nan::New<Integer>(pi.remote_dl_rate));
+        obj->Set(Nan::New("pending_disk_bytes").ToLocalChecked(), Nan::New<Integer>(pi.pending_disk_bytes));
+        obj->Set(Nan::New("send_quota").ToLocalChecked(), Nan::New<Integer>(pi.send_quota));
+        obj->Set(Nan::New("receive_quota").ToLocalChecked(), Nan::New<Integer>(pi.receive_quota));
+        obj->Set(Nan::New("rtt").ToLocalChecked(), Nan::New<Integer>(pi.rtt));
+        obj->Set(Nan::New("num_pieces").ToLocalChecked(), Nan::New<Integer>(pi.num_pieces));
+        obj->Set(Nan::New("download_rate_peak").ToLocalChecked(), Nan::New<Integer>(pi.download_rate_peak));
+        obj->Set(Nan::New("upload_rate_peak").ToLocalChecked(), Nan::New<Integer>(pi.upload_rate_peak));
+        obj->Set(Nan::New("progress").ToLocalChecked(), Nan::New<Number>(pi.progress));
+        obj->Set(Nan::New("progress_ppm").ToLocalChecked(), Nan::New<Integer>(pi.progress_ppm));
+        obj->Set(Nan::New("estimated_reciprocation_rate").ToLocalChecked(), Nan::New<Integer>(pi.estimated_reciprocation_rate));
+        obj->Set(Nan::New("local_endpoint").ToLocalChecked(), get_tcp_endpoint(pi.local_endpoint));
 
-    return scope.Close(obj);
-  }
-
-  void bind_peer_info(Handle<Object> target) {
-    // set flags
-    Local<Object> obj = Object::New();
-
-    obj->Set(String::NewSymbol("interesting"), Integer::New(libtorrent::peer_info::interesting));
-    obj->Set(String::NewSymbol("choked"), Integer::New(libtorrent::peer_info::choked));
-    obj->Set(String::NewSymbol("remote_interested"), Integer::New(libtorrent::peer_info::remote_interested));
-    obj->Set(String::NewSymbol("remote_choked"), Integer::New(libtorrent::peer_info::remote_choked));
-    obj->Set(String::NewSymbol("supports_extensions"), Integer::New(libtorrent::peer_info::supports_extensions));
-    obj->Set(String::NewSymbol("local_connection"), Integer::New(libtorrent::peer_info::local_connection));
-    obj->Set(String::NewSymbol("handshake"), Integer::New(libtorrent::peer_info::handshake));
-    obj->Set(String::NewSymbol("connecting"), Integer::New(libtorrent::peer_info::connecting));
-    obj->Set(String::NewSymbol("on_parole"), Integer::New(libtorrent::peer_info::on_parole));
-    obj->Set(String::NewSymbol("seed"), Integer::New(libtorrent::peer_info::seed));
-    obj->Set(String::NewSymbol("optimistic_unchoke"), Integer::New(libtorrent::peer_info::optimistic_unchoke));
-    obj->Set(String::NewSymbol("snubbed"), Integer::New(libtorrent::peer_info::snubbed));
-    obj->Set(String::NewSymbol("upload_only"), Integer::New(libtorrent::peer_info::upload_only));
-    obj->Set(String::NewSymbol("endgame_mode"), Integer::New(libtorrent::peer_info::endgame_mode));
-    obj->Set(String::NewSymbol("holepunched"), Integer::New(libtorrent::peer_info::holepunched));
-#ifndef TORRENT_DISABLE_ENCRYPTION
-    obj->Set(String::NewSymbol("rc4_encrypted"), Integer::New(libtorrent::peer_info::rc4_encrypted));
-    obj->Set(String::NewSymbol("plaintext_encrypted"), Integer::New(libtorrent::peer_info::plaintext_encrypted));
-#endif
-
-    obj->Set(String::NewSymbol("standard_bittorrent"), Integer::New(libtorrent::peer_info::standard_bittorrent));
-    obj->Set(String::NewSymbol("web_seed"), Integer::New(libtorrent::peer_info::web_seed));
-
-    obj->Set(String::NewSymbol("tracker"), Integer::New(libtorrent::peer_info::tracker));
-    obj->Set(String::NewSymbol("dht"), Integer::New(libtorrent::peer_info::dht));
-    obj->Set(String::NewSymbol("pex"), Integer::New(libtorrent::peer_info::pex));
-    obj->Set(String::NewSymbol("lsd"), Integer::New(libtorrent::peer_info::lsd));
-    obj->Set(String::NewSymbol("resume_data"), Integer::New(libtorrent::peer_info::resume_data));
-    obj->Set(String::NewSymbol("incoming"), Integer::New(libtorrent::peer_info::incoming));
-
-    obj->Set(String::NewSymbol("bw_idle"), Integer::New(libtorrent::peer_info::bw_idle));
-    obj->Set(String::NewSymbol("bw_limit"), Integer::New(libtorrent::peer_info::bw_limit));
-    obj->Set(String::NewSymbol("bw_network"), Integer::New(libtorrent::peer_info::bw_network));
-    obj->Set(String::NewSymbol("bw_disk"), Integer::New(libtorrent::peer_info::bw_disk));
-
-    target->Set(String::NewSymbol("peer_info"), obj);
-  };
+        return scope.Escape(obj);
+    }
 }; // namespace nodelt
--- src/peer_info.hpp
+++ src/peer_info.hpp
@@ -5,9 +5,13 @@
 
 #include <libtorrent/peer_info.hpp>
 
+using namespace v8;
 
 namespace nodelt {
-  v8::Local<v8::Object> peer_info_to_object(const libtorrent::peer_info& pi);
+    void bind_peer_info(Local<Object> target);
+    Local<Array> get_tcp_endpoint(const libtorrent::tcp::endpoint& ep);
+    Local<Object> peer_info_to_object(const libtorrent::peer_info& pi);
+    Local<Array> get_pieces(const libtorrent::peer_info& pi);
 };
 
 #endif // NODE_LIBTORRENT_PEER_INFO_PARAMS_HPP_INCLUDED
